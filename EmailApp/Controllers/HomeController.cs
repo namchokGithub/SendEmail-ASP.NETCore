d7ab4b33d924f1bb0e9063a7acd0653f21ad9b9f
Namchok Singhachai <44488002+namchokGithub@users.noreply.github.com>
2020-08-26 11:04:22 +0700

    add send message

--- EmailApp/Controllers/HomeController.cs
+++ EmailApp/Controllers/HomeController.cs
@@ -6,20 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EmailApp.Models;
+using EmailService;
 
 namespace EmailApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IEmailSender _emailSender;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IEmailSender emailSender)
         {
-            _logger = logger;
+            _emailSender = emailSender;
         }
 
         public IActionResult Index()
         {
+
+            var message = new Message(
+                    new string[] { "60160169@go.buu.ac.th" },
+                    "So hungy",
+                    "This message is the content from MHEE"
+                );
+
+            _emailSender.SendEmail(message);
+
             return View();
         }
 
